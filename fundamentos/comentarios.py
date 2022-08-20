202b2026cf3f9b0a2bc4aa94fc8d946dc93c9ccc
William Viana <willdev.viana@gmail.com>
2022-08-20 18:38:24 -0300

    understand comments

---
+++ fundamentos/comentarios.py
@@ -0,0 +1,17 @@
+# Minhas variáveis
+salario = 4000.00
+despesas = 2000.00
+
+'''
+Calcular quanto vai sobrar no final do mês
+'''
+
+print(salario - despesas)
+
+'''
+  Comentários devem ser inseridos em cima do trecho de código.
+  Ter um código com bons nomes de variáveis, funções e classe, 
+  o código fica mais claro e evita comentários desnecessários.
+'''
+
+print("Fim")
