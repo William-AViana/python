9ca3aa34e6c2db8e4398a1b5026fedd24c86d766
William Viana <willdev.viana@gmail.com>
2022-08-19 22:49:19 -0300

    basic types on python

---
+++ fundamentos/tiposBasicos.py
@@ -0,0 +1,10 @@
+print(True)
+print(False)
+print(1.2 + 1)
+print('Aqui eu falo naminha língua!')
+print("Tb funciona.")
+print('Eu sou programador ' + 3 * 'muito ' + 'bom.')
+# print('3' + 3)  # ambiguidade
+print([1, 2, 3])  # lista
+print({'nome': 'Pedro', 'idade': 32})
+print(None)
