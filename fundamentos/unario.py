61caa980d64b4f7420788b499c598b43d5727d11
William Viana <willdev.viana@gmail.com>
2022-08-23 21:43:02 -0300

    operadores unarios e como funcionam

---
+++ fundamentos/unario.py
@@ -0,0 +1,20 @@
+a = 3
+# Operadores não funcionais em Python
+# print(a++)
+# print(a += 1)
+# print(a--)
+
+# Operador sem resultado não é possível incrementar
+print(++a)
+
+# tornar o número negativo
+print(-a)
+
+# tornar o número positivo
+print(+a)
+
+print(not 0)
+print(not 1)
+print(not -2)
+print(not False)
+print(not not True)
