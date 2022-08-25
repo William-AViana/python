b540a86dec772de19ed3ab511b341bd4a98a3b4f
William Viana <willdev.viana@gmail.com>
2022-08-25 17:02:14 -0300

    entendendo conversao de tipos

---
+++ fundamentos/conversao_de_tipos.py
@@ -0,0 +1,17 @@
+print(2 + 3)
+print('2' + '3')
+
+# print(2 + '3')
+
+a = 2
+b = '3'
+
+print(type(a))
+print(type(b))
+
+print(a + int(b))
+print(str(a) + b)
+
+print(a + float('3.5'))
+
+# a string de fato tm que estar formatada para ser convertida.
