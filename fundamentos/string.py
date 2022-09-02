ae36ab4e5e2674de056101ddc0f9201c145c7f7e
William Viana <willdev.viana@gmail.com>
2022-09-02 20:36:49 -0300

    strings e suas características

---
+++ fundamentos/string.py
@@ -0,0 +1,17 @@
+nome = 'Saulo Pedro'
+
+print(nome)
+print(nome[0])
+# print('Marca d'agua')
+print("marca d'agua")
+print('Teste \" funciona')
+texto = 'Texto entre apóstrofos pode ter "aspas"'
+print(texto)
+doc = """Texto com múltiplas
+...linhas"""
+print(doc)
+print('Texto com múltiplas\n\t... linhas')
+
+doc2 = '''Também é possível
+... com aspas simples'''
+print(doc2)
