33f23561ed604c224aac1817c707aece0ed2ff65
William Viana <willdev.viana@gmail.com>
2022-09-07 22:25:41 -0300

    manipulando dados de em formato lista

---
+++ fundamentos/lista.py
@@ -0,0 +1,18 @@
+lista = []
+
+print(type(lista))
+# print(dir(lista))
+# print(help(list))
+print(len(lista))
+lista.append(1)
+lista.append(5)
+lista.append([6, 7, 8, 9])
+print(lista)
+print(len(lista))
+
+nova_lista = [1, 2, 'William', 'Grazi']
+print(nova_lista)
+nova_lista.remove(1)
+print(nova_lista)
+nova_lista.reverse()
+print(nova_lista)
