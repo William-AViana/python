561ba0dccab6d73675e4ee8905546ec9cb8ecd1e
William Viana <willdev.viana@gmail.com>
2022-09-07 22:22:01 -0300

    consultando e manipulando string

---
+++ fundamentos/string3.py
@@ -0,0 +1,17 @@
+frase = 'Python é uma linguagem interessante'
+
+print('py' in frase)
+print('ing' in frase)
+
+print(len(frase))
+print('py' in frase.lower())
+
+print(frase)
+print(frase.upper())
+
+frase = frase.upper()
+print(frase.split())
+print(frase.split('E'))
+
+print(dir(str))
+# print(help(str.center))
