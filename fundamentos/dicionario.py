e2b3fe7dfeeefc4296cc4bbafbfda3b5b425d683
William Viana <willdev.viana@gmail.com>
2022-09-10 00:14:17 -0300

    finalizando dictionary e acessando valores

--- fundamentos/dicionario.py
+++ fundamentos/dicionario.py
@@ -3,3 +3,16 @@ pessoa = {'nome': 'william', 'idade': 32, 'cursos': [ 'ADS', 'Inglês']}
 print(type(pessoa))
 # print(dir(pessoa))
 print(len(pessoa))
+
+print(pessoa)
+print(pessoa['nome'])
+print(pessoa['idade'])
+print(pessoa['cursos'][1])
+# print(pessoa['tags']) # Key error
+
+print(pessoa.keys())
+print(pessoa.values())
+print(pessoa.items())
+print(pessoa.get('idade'))
+print(pessoa.get('tags'))
+print(pessoa.get('tags', []))
