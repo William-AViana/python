46ffc6583c63ae5b9df14c41183d2482ca7d9417
William Viana <willdev.viana@gmail.com>
2022-09-28 22:29:29 -0300

    for com continue e break

---
+++ estrutura_controle/break_continue.py
@@ -0,0 +1,14 @@
+from ast import Break
+
+
+for x in range(1, 11):
+    if x % 2 == 0:
+        continue
+    print(x)
+
+for x in range(1, 11):
+    if x == 5:
+        break
+    print(x)
+
+print('Fim')
