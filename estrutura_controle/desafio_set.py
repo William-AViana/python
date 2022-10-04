38c160c2853f0de4551c25cd96e40d9e6a04dab6
William Viana <willdev.viana@gmail.com>
2022-10-03 22:07:20 -0300

    finaly challenge seach  words in frase

--- estrutura_controle/desafio_set.py
+++ estrutura_controle/desafio_set.py
@@ -4,3 +4,10 @@ textos = [
     'Jão gosta de futebol e política',
     'A praia foi divertida',
 ]
+
+for texto in textos:
+    intersecao = PALAVRAS_PROIBIDAS.intersection(set(texto.lower().split()))
+    if intersecao:
+        print('Texto possui palavras proibidas', intersecao)
+    else:
+        print('Texto autorizado:', texto)
