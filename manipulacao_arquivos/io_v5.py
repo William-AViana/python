8241c0cde91ebb76c60ad215b88e53329acd185d
William Viana <willdev.viana@gmail.com>
2022-10-10 21:42:53 -0300

    read file with block WITH for close file automatically

---
+++ manipulacao_arquivos/io_v5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# The resource allocated inside the block will be closed
+with open('pessoas.csv') as arquivo:
+    for registro in arquivo:
+        print('Nome: {}, Idade: {}'.format(*registro.strip().split(',')))
+
+if arquivo.closed:
+    print('File closed!')
