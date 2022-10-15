3d83284fe83bbbd44b5e79085437eec94106b8a8
William Viana <willdev.viana@gmail.com>
2022-10-15 20:06:53 -0300

    code for generate packing and string with functions

--- funcoes/gerador_html_v3.py
+++ funcoes/gerador_html_v3.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def tag_bloco(conteudo, classe='success', inline=False):
+    tag = 'span' if inline else 'div'
+    return f'<{tag} class="{classe}">{conteudo}</{tag}>'
+
+
+def tag_lista(*itens):
+    lista = ''.join(f'<li>{iten}</li>' for iten in itens)
+    return f'<ul>{lista}</ul>'
+
+
+if __name__ == '__main__':
+    print(tag_bloco(tag_lista('item 1', 'item 2'), classe='info'))
