fbd9836f4174437f8ffbd164b80b28f6b889529b
William Viana <willdev.viana@gmail.com>
2022-10-24 21:07:38 -0300

    create structure for challenge

---
+++ funcoes/desafio_html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def tag(tag, *args, **kwargs):
+    pass
+
+
+if __name__ == '__main__':
+    print(
+        tag('p',
+            tag('apan', 'Curso de Python, por '),
+            tag('strong', 'Juracy', id='j'),
+            tag('span', ' e '),
+            tag('strong', 'Leonardo', id='l'),
+            tag('span', '.'),
+            html_class='alert')
+    )
