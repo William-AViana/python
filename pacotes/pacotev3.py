61e04643c2a22689d74c769a957f32c8a1f72e3d
William Viana <willdev.viana@gmail.com>
2022-10-25 21:57:19 -0300

    import package with alias

--- pacotes/pacotev3.py
+++ pacotes/pacotev3.py
@@ -1,3 +1,5 @@
-from pacote1 import modulo2
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
 
-modulo2.main()
+print('Soma', modulo1.soma(3, 6))
+print('Subtração', modulo1_sub.subtracao(9, 3))
